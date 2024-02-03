3e850a16c40ddf33c6502c213b09c7544490419a
Daniel-Abrams <151103373+Daniel-Abrams@users.noreply.github.com>
2024-02-03 12:52:39 -0700

    Add files via upload

---
+++ MMC5983MA_Magnetometer_Code.ino
@@ -0,0 +1,72 @@
+
+//Compute magnetic heading from the MMC5983MA
+
+//Required Libraries
+#include <Wire.h>
+#include <SparkFun_MMC5983MA_Arduino_Library.h> //Click here to get the library: http://librarymanager/All#SparkFun_MMC5983MA
+
+SFE_MMC5983MA myMag;                            //Magnetometer name
+
+void setup()
+{
+    Serial.begin(115200);                       //Set baud rate to 115200
+    Serial.println("MMC5983MA Magnetometer");
+
+    Wire.begin();
+
+    if (myMag.begin() == false)
+    {
+        Serial.println("MMC5983MA did not respond - check your wiring. Freezing.");
+        while (true)
+            ;
+    }
+
+    myMag.softReset();
+
+    Serial.println("MMC5983MA connected");
+}
+
+void loop()
+{
+    Serial.print("Heading: ");
+    Serial.println(getHeading(), 1);
+    delay(100);
+}
+
+
+double getHeading(){
+  
+    uint32_t rawValueX = 0;
+    uint32_t rawValueY = 0;
+    uint32_t rawValueZ = 0;
+    double scaledX = 0;
+    double scaledY = 0;
+    double scaledZ = 0;
+    double heading = 0;
+
+    // Read all three channels simultaneously
+    myMag.getMeasurementXYZ(&rawValueX, &rawValueY, &rawValueZ);
+
+    // The magnetic field values are 18-bit unsigned. The _approximate_ zero (mid) point is 2^17 (131072).
+    // Here we scale each field to +/- 1.0 to make it easier to calculate an approximate heading.
+   
+    scaledX = (double)rawValueX - 131072.0;
+    scaledX /= 131072.0;
+
+    scaledY = (double)rawValueY - 131072.0;
+    scaledY /= 131072.0;
+
+    scaledZ = (double)rawValueZ - 131072.0;
+    scaledZ /= 131072.0;
+
+    // Magnetic north is oriented with the Y axis
+    // Convert the X and Y fields into heading using atan2 (Arc Tangent 2)
+    heading = atan2(scaledX, 0 - scaledY);
+
+    // atan2 returns a value between +PI and -PI
+    // Convert to degrees
+    heading /= PI;
+    heading *= 180;
+
+    return heading;
+}
